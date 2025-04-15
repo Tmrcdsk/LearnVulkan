10c860bf7a5f8f3267bb449ad43c7b34596d5a39
Shushu <1345254469@qq.com>
2025-04-15 17:11:42 +0800

    载入着色器

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <stdexcept>
 #include <cstdlib>
@@ -370,7 +371,8 @@ private:
 
 	void createGraphicsPipeline()
 	{
-
+		auto vertShaderCode = readFile("res/shaders/vert.spv");
+		auto fragShaderCode = readFile("res/shaders/frag.spv");
 	}
 
 	VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats)
@@ -543,6 +545,24 @@ private:
 		}
 
 		return true;
+	}
+
+	static std::vector<char> readFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+		return buffer;
 	}
 
 	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
