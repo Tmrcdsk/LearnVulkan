c26a22294b8dde6fb78ae04f627f4e1d701e8c34
Shushu <1345254469@qq.com>
2025-04-17 10:58:14 +0800

    指令缓冲(Command buffers)：分配指令缓冲

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -97,6 +97,7 @@ private:
 	VkPipeline graphicsPipeline;
 
 	VkCommandPool commandPool;
+	VkCommandBuffer commandBuffer;
 
 	void initWindow() {
 		glfwInit();
@@ -121,6 +122,7 @@ private:
 		createGraphicsPipeline();
 		createFramebuffers();
 		createCommandPool();
+		createCommandBuffers();
 	}
 
 	void mainLoop() {
@@ -581,6 +583,19 @@ private:
 
 		if (vkCreateCommandPool(device, &poolInfo, nullptr, &commandPool) != VK_SUCCESS) {
 			throw std::runtime_error("failed to create command pool!");
+		}
+	}
+
+	void createCommandBuffers()
+	{
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.commandPool = commandPool;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandBufferCount = 1;
+
+		if (vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate command buffers!");
 		}
 	}
 
