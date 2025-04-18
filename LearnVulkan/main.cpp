906995aa4357192420ca9ff92f417a23c8e1aef9
Tmrcdsk <129770406+Tmrcdsk@users.noreply.github.com>
2025-04-18 14:52:14 +0800

    顶点缓冲区：属性描述

--- LearnVulkan/main.cpp
+++ LearnVulkan/main.cpp
@@ -12,6 +12,7 @@
 #include <vector>
 #include <cstring>
 #include <limits>
+#include <array>
 #include <optional>
 #include <set>
 
@@ -79,6 +80,22 @@ struct Vertex
 		bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 
 		return bindingDescription;
+	}
+
+	static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+		std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+		attributeDescriptions[0].binding = 0;
+		attributeDescriptions[0].location = 0;
+		attributeDescriptions[0].format = VK_FORMAT_R32G32_SFLOAT;
+		attributeDescriptions[0].offset = offsetof(Vertex, pos);
+
+		attributeDescriptions[1].binding = 1;
+		attributeDescriptions[1].location = 1;
+		attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+		attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+		return attributeDescriptions;
 	}
 };
 
